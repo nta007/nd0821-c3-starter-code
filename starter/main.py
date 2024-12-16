4d6fe7ee7bac3f97b04d622e8cbb72d5d6027ed9
jobuta93 <saka007@gmai.com>
2024-12-16 23:49:01 +0700

    exclude __pycache__

--- starter/main.py
+++ starter/main.py
@@ -1,11 +1,13 @@
-# Put the code for your API here.
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI(version="0.1.0", title="Udacity DevOps ML API",
               description="API for ML model")
 
-
 if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+
+@app.route('/')
+def home():
+    return {"message": "Hello World!"}
