2cf6abd346f48df99fd954ccfbe15fa8d8d25f34
anhnt203 <saka007@gmai.com>
2024-12-16 23:45:12 +0700

    init project

---
+++ conda.create-env.sh
@@ -0,0 +1,1 @@
+conda create -n devops-ml-3 "python=3.8" scikit-learn pandas numpy pytest jupyter jupyterlab fastapi uvicorn -c conda-forge
--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,11 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(version="0.1.0", title="Udacity DevOps ML API",
+              description="API for ML model")
+
+
+if __name__ == "__main__":
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
