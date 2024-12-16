a0f60672694bc81e67d4b08c40c00135ceef0f40
anhnt203 <saka007@gmai.com>
2024-12-16 23:59:12 +0700

    api: hello-world

--- starter/main.py
+++ starter/main.py
@@ -4,10 +4,13 @@ import uvicorn
 app = FastAPI(version="0.1.0", title="Udacity DevOps ML API",
               description="API for ML model")
 
+@app.get('/')
+def hello_world():
+    return {"message": "Hello World!"}
+
+@app.post('/predict')
+def predict():
+    return {"message": "Model prediction"}
+
 if __name__ == "__main__":
     uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
-
-
-@app.route('/')
-def home():
-    return {"message": "Hello World!"}
